7d65dccbe875faccaf7959911099616c40ccc780
LaetitiaCouge <44775082+LaetitiaCouge@users.noreply.github.com>
2019-12-11 11:55:34 +0100

    Copy the resize pict in train folder

---
+++ Preprocessing/copy_resize_pict.py
@@ -0,0 +1,23 @@
+#!/bin/bash
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from shutil import copyfile
+
+
+
+if __name__== "__main__":                
+    path_train = './data/train/'  
+    path_non_square = './data/non_square/'  
+    path_resize = './data/resized/'     
+    
+    print(os.getcwd())
+    
+    class_plants = os.listdir(path_resize)
+    for cd in class_plants:  
+        class_list_plants = os.listdir(path_resize + cd)
+        for img_plant in class_list_plants:    
+            print(path_resize+cd+'/'+img_plant)
+            #print(path_train+cd+'/'+img_plant)
+            copyfile(path_resize+cd+'/'+img_plant, path_train+cd+'/'+img_plant)
